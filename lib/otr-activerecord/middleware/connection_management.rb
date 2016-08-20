648db9952e8d17ec217cab57bb02e331c4e6a04b
Jordan Hollinger <jordan.hollinger@gmail.com>
2016-08-20 11:19:32 -0400

    Add OTR::ActiveRecord::ConnectionManagement middleware

---
+++ lib/otr-activerecord/middleware/connection_management.rb
@@ -0,0 +1,26 @@
+module OTR
+  module ActiveRecord
+    #
+    # Rack middleware that returns active db connections to the connection pool after a request completes.
+    #
+    class ConnectionManagement
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+        testing = env['rack.test'] == true
+
+        resp = @app.call env
+        resp[2] = ::Rack::BodyProxy.new resp[2] do
+          ActiveRecord::Base.clear_active_connections! unless testing
+        end
+        resp
+
+      rescue Exception
+        ActiveRecord::Base.clear_active_connections! unless testing
+        raise
+      end
+    end
+  end
+end
