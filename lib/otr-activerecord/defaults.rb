a9dbeba6455aa360246def73ab1df9f0a8120b2d
Jordan Hollinger <jordan.hollinger@gmail.com>
2018-12-11 09:34:59 -0500

    Default env to development (Bugfix for Rails 5.2)

--- lib/otr-activerecord/defaults.rb
+++ lib/otr-activerecord/defaults.rb
@@ -1,3 +1,6 @@
+ENV['RACK_ENV'] ||= OTR::ActiveRecord.rack_env.to_s
+ENV['RAILS_ENV'] ||= OTR::ActiveRecord.rack_env.to_s
+
 OTR::ActiveRecord.db_dir = 'db'
 OTR::ActiveRecord.migrations_paths = %w(db/migrate)
 OTR::ActiveRecord.fixtures_path = 'test/fixtures'
